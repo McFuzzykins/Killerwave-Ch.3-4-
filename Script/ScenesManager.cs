e50b5c7dbdd0dbd931cf514e5964f16a4b612860
McFuzzykins <77823726+McFuzzykins@users.noreply.github.com>
2021-03-29 09:04:45 -0700

    Add files via upload

---
+++ Script/ScenesManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Scenes scenes;
+    public enum Scenes
+    {
+        bootUp,
+        title,
+        shop,
+        level1,
+        level2,
+        level3,
+        gameOver
+    }
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void GameOver()
+    {
+        SceneManager.LoadScene("gameOver");
+        Debug.Log("ENDSCORE: " + GameManager.Instance.GetComponent<ScoreManager>().PlayerScore);
+    }
+    public void BeginGame()
+    {
+        SceneManager.LoadScene("testLevel");
+    }
+}
